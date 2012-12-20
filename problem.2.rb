5fecfe67fa0c290cca1f0fcae4c0d6f1b110d2fb
Hash <takuya21hashimoto@gmail.com>
2012-12-20 10:41:07 +0900

    solve 2

--- problem.2.rb
+++ problem.2.rb
@@ -1,11 +1,19 @@
 # coding: utf-8
 
-def fib(n) # memorized version
+def fib(n) # memorized version is better.
   n == 1 ? 1 : n == 2 ? 2 : fib(n - 1) + fib(n - 2)
 end
-def solve(max)
-  map{|i| fib(i) }.
-  select{|m| m % 2 == 0 }
-  inject(0){|sum,i| sum += i }
+def fib_list_under(max)
+  ary = []
+  (1...max).each do |i|
+    return ary if (k = fib(i)) >= max
+    ary << k
+  end
 end
-p solve(6)
+def solve(max)
+  fib_list_under(max).
+    select{|n| n % 2 == 0 }.
+    #tap{|t| p t }.
+    inject(0){|sum,m| sum += m }
+end
+p solve(4000000)
